6b19b859f13c9d268efbb882489b555f21eed1b8
fangimal <gimalfan@gmail.com>
2025-09-16 00:40:42 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ChatMicroservices
+
+
+
+
---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+// Package main запускает основное приложение чата.
+package main
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println(color.GreenString("Hello, world!"))
+}
